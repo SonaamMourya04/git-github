2859d70b73a191f778551b86f1f520e2b69126f1
SonaamMourya04 <sonammourya7683@gmail.com>
2026-04-11 14:06:21 +0530

    addded a new line of code

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log(" Hello, World! This is a simple Node.js application.");
+console.log(" It demonstrates how to create a basic Node.js application and run it.");
+console.log(" You can run this application using the command: node index.js");
+console.log(" Enjoy learning Node.js and building amazing applications!");          
